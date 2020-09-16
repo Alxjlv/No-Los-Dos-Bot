3f1d67db21c485c17df391af992b58eae7358e7a
Alex Verkerk <44216323+Alxjlv@users.noreply.github.com>
2020-09-16 23:55:10 +1200

    Added trigger for when the bot is mentioned

--- bot/main.py
+++ bot/main.py
@@ -13,25 +13,26 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    await do_the_thing(message)
+    print(client.user)
+    strings = {'why not both', 'por que no los dos', 'both', 'no los dos', 'por que', 'no los', 'dos', 'los', 'por',
+               'que', 'qué', '<@!755731316766670868>'}
+    print(message.content)
+    msg = message.content.lower()
+    for s in strings:
+        if s in msg:
+            await do_the_thing(message)
     return
 
 
 async def do_the_thing(message):
-    strings = {'why not both', 'por que no los dos', 'both', 'no los dos', 'por que', 'no los', 'dos', 'los', 'por',
-               'que', 'qué'}
-    msg = message.content.lower()
-    for s in strings:
-        if s in msg:
-            script_dir = os.path.dirname(__file__)
-            rel_path = r"porquenolosdos.gif"
-            abs_file_path = os.path.join(script_dir, rel_path)
-            await message.add_reaction('\U0001F467')
-            await message.add_reaction('\U0001F334')
-            await message.add_reaction('\U0001F389')
-            await message.channel.send('¿Por qué no los dos?', file=discord.File(abs_file_path))
-            return
+    script_dir = os.path.dirname(__file__)
+    rel_path = r"porquenolosdos.gif"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    await message.add_reaction('\U0001F467')
+    await message.add_reaction('\U0001F334')
+    await message.add_reaction('\U0001F389')
+    await message.channel.send('¿Por qué no los dos?', file=discord.File(abs_file_path))
+    return
 
 
 try:
